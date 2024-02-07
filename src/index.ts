88e9cb43606d243108bd407b3898b2b5010dd44c
Adrian <adrian.krause.ak@gmail.com>
2024-02-07 21:04:14 +0100

    Created products /get endpoint with pagination.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import express, { Express } from 'express';
+import routes from './routes/routes.js';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+app.use(cors());
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
---
+++ src/services/productsService.ts
@@ -0,0 +1,12 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const read = async() => {
+  const fileLocation = path.resolve('public/api', 'products.json');
+  const data = await fs.readFile(fileLocation, 'utf-8');
+  return JSON.parse(data);
+};
+
+export const getAll = async() => {
+  return read();
+};
