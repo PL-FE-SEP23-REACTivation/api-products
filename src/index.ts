df7ecb2111f1787f009c73eac5dd8f9ae9e019e7
Marcin Ligmann <marcinligmann2@gmail.com>
2024-02-09 14:23:05 +0100

    working

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,20 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import express, { Express } from 'express';
 import routes from './routes/routes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use('/', routes);
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
