00b9b837c48976a412e98e8542852a29bebcf31e
Laura Wronska <laura.wronskaa@gmail.com>
2024-02-09 12:44:55 +0100

    fix formatting

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,14 +3,16 @@ import { Sequelize } from 'sequelize-typescript';
 
 dotenv.config();
 
-const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 // eslint-disable-next-line max-len
 const URI = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}.frankfurt-postgres.render.com/${DB_NAME}`;
 
-export const sequelize = new Sequelize(URI, { dialect: 'postgres',
+export const sequelize = new Sequelize(URI, {
+  dialect: 'postgres',
   dialectOptions: {
     ssl: true,
-  }});
+  },
+});
 
 export async function connect() {
   try {
