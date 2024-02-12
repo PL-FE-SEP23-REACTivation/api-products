dba915ca279a4f3fee838f902e3e10d5666f00f0
Laura Wronska <laura.wronskaa@gmail.com>
2024-02-13 00:48:05 +0100

    init first migration for products table

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,18 +1,47 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import dotenv from 'dotenv';
-import { Sequelize } from 'sequelize-typescript';
+import { Sequelize, SequelizeOptions } from 'sequelize-typescript';
 
 dotenv.config();
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-// eslint-disable-next-line max-len
-const URI = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}.frankfurt-postgres.render.com/${DB_NAME}`;
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_HOST,
+  DEV_DB_USER,
+  DEV_DB_PASSWORD,
+  DEV_DB_NAME,
+  DEV_DB_HOST,
+} = process.env;
 
-export const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: true,
-  },
-});
+const settings: SequelizeOptions =
+  process.env.NODE_ENV === 'production'
+    ? {
+      database: DB_NAME,
+      username: DB_USER,
+      password: DB_PASSWORD,
+      host: DB_HOST,
+    }
+    : {
+      database: DEV_DB_NAME,
+      username: DEV_DB_USER,
+      password: DEV_DB_PASSWORD,
+      host: DEV_DB_HOST,
+    };
+
+export const sequelize = new Sequelize(
+  settings.database!,
+  settings.username!,
+  settings.password!,
+  {
+    host: settings.host!,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: process.env.NODE_ENV === 'production',
+    },
+  }
+);
 
 export async function connect() {
   try {
