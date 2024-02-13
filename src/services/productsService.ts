b3f8752bfe5b2ece77771e1387a5796af134f0a2
Marcin Ligmann <marcinligmann2@gmail.com>
2024-02-13 18:17:15 +0100

    working verion of random 10 products from public/api/products.json

--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -1,5 +1,6 @@
 import fs from 'fs/promises';
 import path from 'path';
+import Product from '../../models/product';
 
 const read = async () => {
   const fileLocation = path.resolve('public/api', 'products.json');
@@ -9,4 +10,35 @@ const read = async () => {
 
 export const getAll = async () => {
   return read();
+};
+
+export const getRecomendedProducts = async () => {
+  const allproducts = await getAll();
+  const numb: number[] = await generateRandomNumbersArray(allproducts.length);
+  const result: Product[] = [];
+
+  await numb.forEach(async el => {
+    result.push(allproducts.find((p: Product) => p.id === el));
+  });
+
+  return result;
+};
+
+const generateRandomNumbersArray = (max: number) => {
+  const result: number[] = [];
+
+  if (max <= 0) {
+    return result;
+  }
+
+  while (result.length < 10) {
+    const randomNumber = Math.floor(Math.random() * (max - 1 + 1)) + 1;
+
+    if (result.indexOf(randomNumber) === -1) {
+      result.push(randomNumber);
+    }
+  }
+
+  return result;
+
 };
