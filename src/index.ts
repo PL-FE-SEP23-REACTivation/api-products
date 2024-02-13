2d7412a903bb7a1c4dbdbe161db73b215e96e9e6
Marcin Ligmann <marcinligmann2@gmail.com>
2024-02-13 13:31:40 +0100

    express.static

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,21 @@ import dotenv from 'dotenv';
 import express, { Express } from 'express';
 import routes from './routes/routes.js';
 import { connect } from './utils/db.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 connect();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use('/', routes);
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
