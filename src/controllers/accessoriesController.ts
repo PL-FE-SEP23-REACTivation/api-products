4991aac3caf28323a57b7bf4ecde1b9a8dcb9c1f
Marcin Ligmann <marcinligmann2@gmail.com>
2024-02-17 01:01:35 +0100

    fixed getAll /accessories

--- src/controllers/accessoriesController.ts
+++ src/controllers/accessoriesController.ts
@@ -4,13 +4,10 @@ import * as service from '../services/accessoriesService.js';
 export const getAll = async (req: Request, res: Response) => {
   const page = parseInt(req.query.page as string) || 1;
   const limit = parseInt(req.query.limit as string) || 8;
-
-  const products = await service.getAll();
   const startIndex = (page - 1) * limit;
-  const lastIndex = page * limit;
-  const paginatedProducts = products.slice(startIndex, lastIndex);
+  const products = await service.getAllWithPagination(limit, startIndex);
 
-  res.send(paginatedProducts);
+  res.send(products);
 };
 
 export const getAccessorieById = async (req: Request, res: Response) => {
--- src/services/accessoriesService.ts
+++ src/services/accessoriesService.ts
@@ -1,7 +1,13 @@
 import Accessory from '../../models/accessory.js';
 
-export const getAll = async () => {
-  return await Accessory.findAll();
+export const getAllWithPagination = async (
+  limit: number,
+  startIndex: number
+) => {
+  return await Accessory.findAll({
+    limit,
+    offset: startIndex,
+  });
 };
 
 export const getAccessorie = async (id: string) => {
